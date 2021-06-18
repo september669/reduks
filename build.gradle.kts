18a8b6780ff2ccde8c2dff9812ba6f662e3a99fa
Денис Дорошенко <september669@gmail.com>
2021-06-18 20:15:54 +1000

    Version bumped up 0.1.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ val coroutinesVersion = "1.4.3-native-mt"
 object libVersion{
     private val major = 0
     private val minor = 1
-    private val patch = 4
+    private val patch = 5
     val num = 10_000 * major + 100 * minor + 1 * patch
     val text = "$major.$minor.$patch"
 }
