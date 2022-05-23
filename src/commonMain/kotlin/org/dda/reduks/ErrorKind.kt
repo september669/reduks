7a97dc4d9901ad1e5712d05c30ee01062da629e1
Денис Дорошенко <september669@gmail.com>
2022-05-24 00:38:34 +1000

    fix ErrorKind

--- src/commonMain/kotlin/org/dda/reduks/ErrorKind.kt
+++ src/commonMain/kotlin/org/dda/reduks/ErrorKind.kt
@@ -1,13 +1,13 @@
 package org.dda.reduks
 
-sealed class ErrorKind(open val exception: Exception?) {
-    data class None(override val exception: Exception? = null) : ErrorKind(exception)
-    data class Generic(override val exception: Exception? = null) : ErrorKind(exception)
-    data class Network(override val exception: Exception? = null) : ErrorKind(exception)
-    data class Maintenance(override val exception: Exception? = null) : ErrorKind(exception)
-    data class NotFound(override val exception: Exception? = null) : ErrorKind(exception)
+sealed class ErrorKind(open val exception: Throwable?) {
+    data class None(override val exception: Throwable? = null) : ErrorKind(exception)
+    data class Generic(override val exception: Throwable? = null) : ErrorKind(exception)
+    data class Network(override val exception: Throwable? = null) : ErrorKind(exception)
+    data class Maintenance(override val exception: Throwable? = null) : ErrorKind(exception)
+    data class NotFound(override val exception: Throwable? = null) : ErrorKind(exception)
     data class Api(
-        override val exception: Exception? = null,
+        override val exception: Throwable? = null,
         val httpStatusCode: Int,
         val apiErrorCode: Int
     ) : ErrorKind(exception)
